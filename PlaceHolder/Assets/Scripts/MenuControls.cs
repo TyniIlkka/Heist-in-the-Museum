d8a40bcbdd2d639367eda8a8472eac4a1759799a
Esa Nord <esa.nord@cs.tamk.fi>
2018-02-01 16:02:45 +0200

    MainMenu Base
    Added Main menu scene, script for menu controls  and readded texture file with placeholder key texture. Also updated ignore list since some files in already ignored .vs folder are added.

---
+++ PlaceHolder/Assets/Scripts/MenuControls.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class MenuControls : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject m_goMenuView1;
+    [SerializeField]
+    private GameObject m_goMenuView2;
+    [SerializeField]
+    private Slider m_sSfxVol;
+    [SerializeField]
+    private Slider m_sMusicVol;
+
+    private void Awake()
+    {
+        // TODO update volume
+    }
+
+    public void NewGame()
+    {
+
+    }
+
+    public void Options()
+    {
+
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void MusicVol()
+    {
+
+    }
+
+    public void SFXVol()
+    {
+
+    }
+
+
+    // TODO pause menu controls?
+}
