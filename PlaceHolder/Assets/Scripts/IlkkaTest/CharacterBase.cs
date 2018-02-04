ba6b100ac62b1e1c8528a62bc6a9d101fe1e4191
Ilkka <ilkka.tyni@cs.tamk.fi>
2018-02-04 21:35:29 +0200

    Player and CharacterBase and Movement
    Player movement implemented to test 2 options

---
+++ PlaceHolder/Assets/Scripts/IlkkaTest/CharacterBase.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProjectThief
+{
+    public abstract class CharacterBase : MonoBehaviour
+    {
+        /// <summary>
+        /// Forces to every character to have Move Method();
+        /// </summary>
+        public abstract void Move(float amount);
+
+    }
+}
---
+++ PlaceHolder/Assets/Scripts/IlkkaTest/Player.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProjectThief {
+    public class Player : CharacterBase
+    {
+
+        //Testing different movements boolean
+        public bool m_bRotateWithKeyboard;
+
+        [SerializeField]
+        private string m_sHorizontalAxis = "Horizontal";
+        [SerializeField]
+        private string m_sVerticalAxis = "Vertical";
+
+        [SerializeField]
+        private float m_fMovementSpeed;
+        [SerializeField]
+        private float m_fTurnSpeed;
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            var input = ReadInput();
+            if (m_bRotateWithKeyboard)
+            {
+                Turn(input.z);
+            }
+            else
+            {
+                MoveLeftRight(input.z);
+            }
+            Move(input.x);
+
+        }
+
+        /// <summary>
+        /// Move method to 
+        /// </summary>
+        public override void Move(float amount)
+        {
+            Vector3 position = transform.position;
+            Vector3 movement = transform.forward * amount * m_fMovementSpeed * Time.deltaTime;
+            position += movement;
+            transform.position = position;
+        }
+
+
+        /// <summary>
+        /// Turn method
+        /// </summary>
+        public void Turn(float amount)
+        {
+            Vector3 rotation = transform.localEulerAngles;
+            rotation.y += amount * m_fTurnSpeed * Time.deltaTime;
+            transform.localEulerAngles = rotation;
+        }
+
+
+
+        /// <summary>
+        /// Move left or right depending from test boolean
+        /// </summary>
+        public void MoveLeftRight(float amount)
+        {
+            Vector3 position = transform.position;
+            Vector3 movement = transform.right * amount * m_fMovementSpeed * Time.deltaTime;
+            position += movement;
+            transform.position = position;
+        }
+
+        /// <summary>
+        /// Input from player
+        /// </summary>
+        /// <returns>Vector3</returns>
+        private Vector3 ReadInput()
+        {
+            float movement = Input.GetAxis(m_sHorizontalAxis);
+            float turn = Input.GetAxis(m_sVerticalAxis);
+            return new Vector3(turn, 0, movement);
+        }
+
+    }
+}
