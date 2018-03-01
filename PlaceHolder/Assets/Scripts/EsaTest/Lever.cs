310263bac3b615f41ce1d93f64ab0abec5367480
Esa Nord <esa.nord@cs.tamk.fi>
2018-03-01 23:06:16 +0200

    Lever and obstacle test animations & updates

--- PlaceHolder/Assets/Scripts/EsaTest/Lever.cs
+++ PlaceHolder/Assets/Scripts/EsaTest/Lever.cs
@@ -6,28 +6,38 @@ namespace ProjectThief
 {
     public class Lever : MonoBehaviour
     {
-        [SerializeField, Tooltip("Obstacle")]
-        private GameObject m_goObstacle;
+        [SerializeField, Tooltip("Obstacle Animator")]
+        private Animator m_aObstacleAnim;
 
         // Set private when done
         public bool m_bBroken;
         public bool m_bInteractable;
         public bool m_bActive;
 
+        private Animator m_aLeverAnim;        
+
+        private void Awake()
+        {
+            m_aLeverAnim = GetComponentInChildren<Animator>();            
+        }
+
         /// <summary>
         /// Detects if mouse is over an object.
         /// </summary>
         private void OnMouseOver()
         {
+            Debug.Log("Lever");
             if (m_bActive)
             {
                 if (!m_bBroken)
                 {
                     if (Input.GetMouseButton(0))
                     {
+                        Debug.Log("Activated");
                         if (m_bInteractable)
                         {
-                            // TODO activate obstacles animation and move it aside.
+                            m_aLeverAnim.SetBool("Activated", true);
+                            m_aObstacleAnim.SetBool("Open", true);
                         }
                         else
                         {
@@ -44,6 +54,11 @@ namespace ProjectThief
             {
                 // TODO mouse animations.
             }
+
+            if (Input.GetKey(KeyCode.R))
+            {
+                ResetLever();
+            }
         }
 
         /// <summary>
@@ -51,7 +66,8 @@ namespace ProjectThief
         /// </summary>
         public void ResetLever()
         {
-            // TODO Reset obstacles animation.
+            m_aLeverAnim.SetBool("Activated", false);
+            m_aObstacleAnim.SetBool("Open", false);
         }
     }    
 }
