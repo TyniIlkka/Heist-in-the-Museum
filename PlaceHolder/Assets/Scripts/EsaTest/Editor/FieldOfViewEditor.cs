57848769ef7f9b41f3c04247d8e4a9487fa77239
Esa Nord <esa.nord@cs.tamk.fi>
2018-03-20 17:08:25 +0200

    Field of view visuliazation initial commit.

---
+++ PlaceHolder/Assets/Scripts/EsaTest/Editor/FieldOfViewEditor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace ProjectThief.Editor
+{
+    [CustomEditor(typeof(FieldOfView))]
+    public class FieldOfViewEditor : UnityEditor.Editor
+    {
+        private void OnSceneGUI()
+        {
+            FieldOfView fow = (FieldOfView)target;
+            Handles.color = Color.white;
+            Handles.DrawWireArc(fow.transform.position, Vector3.up, Vector3.forward, 360, fow.ViewRad);
+        }
+    }
+}
---
+++ PlaceHolder/Assets/Scripts/EsaTest/FieldOfView.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace ProjectThief
+{
+    public class FieldOfView : MonoBehaviour
+    {
+        [SerializeField]
+        private float m_fViewRad;
+        [SerializeField]
+        private float m_fViewAngle;
+
+        public float ViewRad { get { return m_fViewRad; } }
+
+        private void Awake()
+        {
+            // TODO Get view radius and angle from guard;
+        }
+
+        public Vector3 DirFromAngle (float angleInDeg)
+        {
+            Vector3 result = new Vector3(Mathf.Sin(angleInDeg * Mathf.Deg2Rad), 0,
+                Mathf.Cos(angleInDeg * Mathf.Deg2Rad));
+            return result;
+        }
+    }
+}
