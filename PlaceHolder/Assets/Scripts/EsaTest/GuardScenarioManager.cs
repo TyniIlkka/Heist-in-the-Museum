b36c06426dbdf375378020c8ecf20ecf61a60e4c
Esa Nord <esa.nord@cs.tamk.fi>
2018-03-31 22:08:23 +0300

    Base for guard scenarios

---
+++ PlaceHolder/Assets/Scripts/EsaTest/GuardScenarioManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ProjectThief.States;
+
+namespace ProjectThief
+{
+    public class GuardScenarioManager : MonoBehaviour
+    {
+        [SerializeField, Tooltip("Current scene")]
+        private GameStateBase m_sCurrentState;
+
+        private int m_iCurrentPhase;
+
+        private void Awake()
+        {
+            //m_iCurrentPhase = GameManager.instance.currentPhase;
+
+            Init();
+        }
+
+        private void Init()
+        {
+            if (m_sCurrentState.SceneName == "Lobby")
+            {
+                // TODO Lobby scenarios
+            }
+            else if (m_sCurrentState.SceneName == "Room1")
+            {
+                // TODO Room 1 scenarios
+            }
+            else
+            {
+                Debug.LogError("ERROR: Scenarios for scene not found!");
+            }
+        }
+    }
+}
