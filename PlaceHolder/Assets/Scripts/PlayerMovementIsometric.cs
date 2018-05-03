c11e4ccd3609cc6b7337213f2b67dbebe44573b7
Unknown <ilkka.tyni@cs.tamk.fi>
2018-05-03 13:49:46 +0300

    Auto stash before merge of "master" and "origin/master"

--- PlaceHolder/Assets/Scripts/PlayerMovementIsometric.cs
+++ PlaceHolder/Assets/Scripts/PlayerMovementIsometric.cs
@@ -7,18 +7,25 @@ namespace ProjectThief
     public class PlayerMovementIsometric : MonoBehaviour
     {
         [SerializeField]
-        private float _moveSpeed = 5f;
+        private float _moveSpeedWalk = 3f;
+        [SerializeField]
+        private float _moveSpeedSneak = 1f;
+
         [SerializeField]
         private float _gravity = 20f;
         [SerializeField]
         private Transform _playerTransform;
 
+        public float speed;
         private CharacterController _charCont;
         private Vector3 _movement;
         private Animator _playerAnimator;
+        [SerializeField]
+        private Player player;
 
         private void Awake()
         {
+            player = GetComponent<Player>();
             _charCont = GetComponent<CharacterController>();
             _playerAnimator = GetComponentInChildren<Animator>();
         }
@@ -26,13 +33,15 @@ namespace ProjectThief
         private void Update()
         {
             Move();
+            MoveSpeed();
+            speed = player.Speed;
         }
 
         private void Move()
         {
             if (_charCont.isGrounded)
             {
-                _movement = new Vector3(-Input.GetAxis("Vertical"), 0, Input.GetAxis("Horizontal"));
+                _movement = new Vector3(Input.GetAxis("Vertical"), 0, -Input.GetAxis("Horizontal"));
 
                 if (_movement != Vector3.zero)
                 {
@@ -45,8 +54,12 @@ namespace ProjectThief
                         offsetY + 45, _playerTransform.eulerAngles.z);
 
                     _playerAnimator.SetBool("Moving", true);
+                    //TODO: Replace to value
+                    _playerAnimator.SetFloat("Speed", player.Speed);
+                    //
+
                     _movement = transform.forward + transform.right;
-                    _movement *= _moveSpeed;
+                    _movement *= player.Speed;
                 }
                 else
                 {
@@ -60,6 +73,18 @@ namespace ProjectThief
 
             _movement.y -= _gravity * Time.deltaTime;
             _charCont.Move(_movement * Time.deltaTime);
+        }
+
+        private void MoveSpeed()
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                player.Speed = _moveSpeedSneak;
+            }
+            else
+            {
+                player.Speed = _moveSpeedWalk;
+            }
         }
     }
 }
